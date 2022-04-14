fc8b849461bf785eaa0fc4e4d6476d830fdc0faf
Hakob Manukyan <hakob.manukyan@gmail.com>
2022-04-14 00:56:53 -0400

    Added a parser and tasks that can be executed by pipeline

---
+++ tasks/add.py
@@ -0,0 +1,17 @@
+#############################################################
+# Author: Hakob Manukyan
+#
+# This script for execute a task 'add'
+#
+#############################################################
+
+import os
+import ast
+
+import os
+import ast
+
+num1 = int(os.environ['num1'])
+num2 = int(os.environ['num2'])
+
+print(num1 + num2);
---
+++ tasks/print.py
@@ -0,0 +1,13 @@
+#############################################################
+# Author: Hakob Manukyan
+#
+# This script for execute a task 'print'
+#
+#############################################################
+
+import os
+import ast
+
+message = os.environ['message'].strip('"')
+
+print(message);
